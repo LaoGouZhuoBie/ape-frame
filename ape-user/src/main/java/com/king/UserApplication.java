aa570fa44267103130e23304bbf418543bffee5f
king <kingjoyez0@126.com>
2022-10-17 16:10:55 +0800

    Initial commit

---
+++ ape-user/src/main/java/com/king/UserApplication.java
@@ -0,0 +1,20 @@
+package com.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package: com.king
+ * @ClassName: UserApplication
+ * @author: zhangkanglei
+ * @createTime: 2022-10-17 14:46
+ * @Description:
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+
+}
